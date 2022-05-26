38c85a0f8f903200cb561515664d89c378bf46c4
friz25 <frizmob@gmail.com>
2022-05-26 16:38:14 +0300

    ToDo example ready

--- flask_ToDO.py
+++ flask_ToDO.py
@@ -16,4 +16,27 @@ db.create_all() #создали нашу БД
 
 @app.get("/")
 def home():
-    return "Hello, Wolrd MOTHERFUCKER!"
+    todo_list = db.session.query(Todo).all()
+    return render_template("base.html", todo_list=todo_list)
+
+@app.post("/add")
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.get("/update/<int:todo_id>")
+def update(todo_id):
+    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.get("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
